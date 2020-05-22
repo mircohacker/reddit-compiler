b586b91a17b5604a3d34ef617fa32658157dfa47
Mirco Haug <git@mircohaug.de>
2020-05-22 15:08:01 +0200

    move back to react-scripts

--- frontend/src/components/FooterComponent.tsx
+++ frontend/src/components/FooterComponent.tsx
@@ -8,12 +8,6 @@ type Props = OwnProps;
 
 export const FooterComponent: FunctionComponent<Props> = (props) => {
 
-    // <div classNameName="footer">
-    //     <p>The goal of this project was to learn react. All the sources for the site and the API are checked in
-    //         on <a href="https://github.com/mircohaug/reddit-compiler" target="_blank">Github</a>.</p>
-    //     <p>The used api can be explored <a href={config.BACKEND_HOST + "/docs"}>here</a></p>
-    // </div>
-    // <!-- Footer -->
     return (
         <footer className="pt-4 my-md-5 pt-md-5 border-top">
             <div className="row">
@@ -23,7 +17,7 @@ export const FooterComponent: FunctionComponent<Props> = (props) => {
                         tab etc. Therefor I implmented a way to compile a whole series of posts (identified by a common
                         prefix) into one epub file. <br/> Enjoy!</p>
                     <p>The goal of this project is learning react. All the sources for the site and the API are checked
-                        in on <a href="https://github.com/mircohaug/reddit-compiler" target="_blank">Github</a>.</p>
+                        in on <a href="https://github.com/mircohaug/reddit-compiler" target="_blank" rel="noopener noreferrer">Github</a>.</p>
                     <p>The used api can be explored <a href={config.BACKEND_HOST + "/docs"}>here</a></p>
                     <p>This project uses the GPLv3 License. All content belongs to the original authors.</p>
                 </div>
